abd8c9b3cbee47f0c7f4b8a53631fd684039c63e
Jeremy-Krugman <jkrugman@stevens.edu>
2023-04-03 19:21:16 -0400

    more with the date time input

--- src/newEvent.js
+++ src/newEvent.js
@@ -6,22 +6,65 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker'
 import dayjs from 'dayjs';
+import { Button } from '@mui/material';
 
 
 
 const NewEvent= ()=>{
-    const [date,setDate]=useState(dayjs('2023-04-03'));
-useEffect(()=>{
+    const [dates,setDates]=useState([]);
+    const [rangedate,setDateRange] =useState([new Date(),new Date()]);
+    const [error,setErr]=useState(false);
+/*useEffect(()=>{
+    try{
     async function fetchData(){
-    console.log(date);
-    setDate(date)
+    //setDateRange(rangedate);
+    console.log(dates)
+    let newDate=dates.splice();
+    newDate=([...dates,rangedate])
+    setDates(newDate)
+    console.log(dates);
+    setErr(false);
     }fetchData();
-},[date])
-return (<div>
+}
+catch(e){
+    console.log(e)
+    setErr(true);
+}
+},[dates])*/
+if(error){
+    return(<div>
+        <p>Error</p>
+    </div>)
+}
+else{
+/*return (<div>
     <LocalizationProvider dateAdapter={AdapterDayjs}>
         <DateCalendar value={date} onChange={(newDate)=>setDate(newDate)} />
     </LocalizationProvider>
+    <input type='text' id='start'></input>
+</div>)*/
+return(<div>
+    <DateTimeRangePicker onChange={setDateRange} value={rangedate} />
+    <br />
+    <Button onClick={()=>{
+        console.log(rangedate)
+        let newDate=[...dates,rangedate];
+        setDates(newDate)
+        console.log(dates)
+        let inForm= document.getElementById('inputForm')
+        inForm.value=newDate.toString();}} 
+        >
+            Add new date
+        </Button>
+    <Button onClick={()=>{
+        console.log(dates)
+    }}>Print dates</Button>
+    <form>
+        <input type='text' id='inputForm' readOnly required></input> 
+    </form>
+        
 </div>)
-}
+}}
 export default NewEvent;
