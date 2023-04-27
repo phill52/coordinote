c156839251d29424a83b6300feb157c6cca164cc
AughdonBreslin <augh.breslin@gmail.com>
2023-04-27 19:34:55 -0400

    Updated users to include pictures

--- server/data/users.js
+++ server/data/users.js
@@ -2,56 +2,83 @@ const bcrypt=require('bcrypt')
 const saltRounds=11
 const mongoCollections = require('../config/mongoCollections');
 const users = mongoCollections.users
-const events=mongoCollections.events
+const events = mongoCollections.events
 const eventFunctions=require('./events')
 const {ObjectId}=require('mongodb')
 const validation=require('../validation')
 
-const createUser = async(username,password) => {        //when a user registers
-    username=validation.checkUsername(username)
-    const userCollection=await users();
-    if(await userCollection.findOne({username:username})) {
+const createUser = async (username, password) => {
+    username = validation.checkUsername(username)
+    const userCollection = await users();
+    if(await userCollection.findOne({username: username})) {
         throw "Either the username or password is invalid"
     }
-    password=validation.checkPassword(password,false)
-    const hashed_pw=await bcrypt.hash(password,saltRounds)
-    let newUser= {
+    
+    password = validation.checkPassword(password,false)
+    const hashed_pw = await bcrypt.hash(password,saltRounds)
+
+    let newUser = {
         username: username,
         password: hashed_pw,
         events: []
     }
-    const insertUser=await userCollection.insertOne(newUser);
+
+    const insertUser = await userCollection.insertOne(newUser);
     if(!insertUser.acknowledged || !insertUser.insertedId)
         throw new Error("Could not add user")
+    
     return {insertedUser: true, userId: insertUser.insertedId}
 }
 
-const checkUser = async(username,password) => {         //when a user logs in
-    username=validation.checkUsername(username)
-    password=validation.checkPassword(password,true)
-    const userCollection=await users();
-    const user=await userCollection.findOne({username:username})
+const checkUser = async (username, password) => {
+    username = validation.checkUsername(username)
+    password = validation.checkPassword(password,true)
+
+    const userCollection = await users();
+    const user = await userCollection.findOne({username: username})
     if(!user) throw "Either the username or password is invalid"
-    let user_hashed_password=user.password
-    let comparison=await bcrypt.compare(password,user_hashed_password)
+
+    let user_hashed_password = user.password
+    let comparison = await bcrypt.compare(password, user_hashed_password)
+
     if(comparison) return {authenticatedUser: true, userId:user._id}
     throw "Either the username or password is invalid"
 }
 
-const getUsersEvents = async(userId) => {
-    userId=validation.checkId(userId);
-    const userCollection=await users();
-    const user=await userCollection.findOne({_id: new ObjectId(userId)})
-    if(!user) throw "Could not find user with id "+userId
-    let eventsArray=[]
+const addUserPicture = async (userId, picture) => {
+    userId = validation.checkId(userId)
+    picture = validation.checkPicture(picture)
+
+    const userCollection = await users();
+    const updatedUser = await userCollection.updateOne(
+        {_id: new ObjectId(userId)},
+        {$set: {picture: picture}}
+    );
+
+    if(!updatedUser.acknowledged || !updatedUser.modifiedCount)
+        throw "Could not update user picture"
+
+    return await getUserById(userId);
+}
+
+const getUsersEvents = async (userId) => {
+    userId = validation.checkId(userId);
+
+    const userCollection = await users();
+    const user = await userCollection.findOne({_id: new ObjectId(userId)});
+    if(!user) throw `Could not find user with id ${userId}.`;
+    
+    let eventsArray = [];
     for(let i in user.events){
-        eventsArray.push(await eventFunctions.getEventById(user.events[i]))
+        eventsArray.push(await eventFunctions.getEventById(user.events[i]));
     }
-    return eventsArray
+    
+    return eventsArray;
 }
 
 module.exports={
     createUser,
     checkUser,
+    addUserPicture,
     getUsersEvents
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
-const baseRoutes=require('./baseRoutes')
-const eventRoutes=require('./eventRoutes')
+const baseRoutes = require('./baseRoutes');
+const eventRoutes = require('./eventRoutes');
+const userRoutes = require('./users');
 
 const constructorMethod = (app) => {
-    app.use('/api/yourpage/events',eventRoutes)
-    app.use('/api',baseRoutes)
+    app.use('/api/yourpage/events', eventRoutes);
+    app.use('/api', baseRoutes);
+    app.use('/user', userRoutes);
     // app.use('*',(req,res) => {
     //     res.status(404).json({error: "Not Found!"})
     // })
