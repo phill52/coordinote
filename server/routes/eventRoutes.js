cb7186f3094338881954914ca3ad2610133b1ca5
Jeremy-Krugman <jkrugman@stevens.edu>
2023-05-09 14:11:36 -0400

    got rid of some of the old code

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -19,20 +19,6 @@ aws.config.update({
     region: process.env.AWS_region 
 });
 const s3 = new aws.S3();
-// const upload = multer({
-//     storage: multerS3({
-//         s3: s3,
-//         bucket: 'coordinote',
-//         acl: 'public-read',
-//         metadata: function (req, file, cb) {
-//             cb(null, {fieldName: file.fieldname});
-//         },
-//         key: function (req, file, cb) {
-//             cb(null, Date.now().toString())
-//         },
-//         contentType: multerS3.AUTO_CONTENT_TYPE,
-//     })
-// })
 const storage = multer.memoryStorage();
 const upload = multer({ dest: 'uploads/' });
 
